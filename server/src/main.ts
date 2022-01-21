c2059bfc75dbff9783dbc2b84c2be5137c107afa
Davy Vong <24511732+davyvong@users.noreply.github.com>
2022-01-21 13:01:49 -0500

    chore: remove unused global prefix

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
-  app.setGlobalPrefix(process.env.GLOBAL_PREFIX);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors();
   await app.listen(parseInt(process.env.NEST_PORT, 10), process.env.NEST_HOST);
